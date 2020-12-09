5b733728337c466955322f4af6b8b43086c2cd98
Amit Eisinger <amiteisinger@campus.technion.ac.il>
2020-12-09 03:10:03 +0200

    deleted old files

--- web_client/api/init-firebase.ts
+++
@@ -1,5 +0,0 @@
-export const firebaseClient = require('firestore/app')
-import firebaseConfig from './config/firebase.conn.json';
-import * as _ from "firebase/auth";
-import * as __ from "firebase/firestore"
-firebaseClient.initializeApp(firebaseConfig);
