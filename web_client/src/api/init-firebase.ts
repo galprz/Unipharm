4c62963f49ddfa49c2a2152d56378cb424b1ee3e
Amnon Hanuhov <ahanukov@redhat.com>
2021-06-23 10:11:52 +0300

    Drop firebase api support

--- web_client/src/api/init-firebase.ts
+++
@@ -1,8 +0,0 @@
-import firebaseClient from "firebase/app";
-import firebaseConfig from "./config/firebase.conn.json";
-import "firebase/auth";
-import "firebase/firestore";
-firebaseClient.initializeApp(firebaseConfig);
-export const authClient = firebaseClient.auth();
-export const firestoreClient = firebaseClient.firestore();
-export const deleteFlag = firebaseClient.firestore.FieldValue.delete();
